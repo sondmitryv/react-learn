568ad69ee237dfa914ee5ca3299a68c2065b8778
Дмитрий Сон <sondmitryv@gmail.com>
2019-05-26 17:48:39 +0300

    first commit

---
+++ src/components/Posts.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import Post from './Post';
+export default class Posts extends Component{
+    render(){
+        return(
+            <div className="left">
+                <Post alt="nature" src="http://www.youandthemat.com/wp-content/uploads/nature-2-26-17.jpg"/>
+                <Post alt="nature" src="http://www.youandthemat.com/wp-content/uploads/nature-2-26-17.jpg"/>
+            </div>
+        )
+    }
+} 
